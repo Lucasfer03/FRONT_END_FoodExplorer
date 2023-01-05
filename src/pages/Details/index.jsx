42fd16e57e1a676ff18fd8aedf3df72d421665c5
Lucasfer03 <lucasfdacunha@hotmail.com>
2023-01-05 02:23:21 -0300

    start

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles"
+
+export function Details(){
+
+  return (
+    <Container>
+    <h1>teste</h1>
+    </Container>
+  )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export default {
+    COLORS: {
+        BACKGROUND_900: "#1E1E1E;",
+        BACKGROUND_700: "rgba(0, 17, 26, 1);",
+        
+        TITLE: "#E1E1E6;",
+        SUBTITLE: "#E1E1E6;",
+        TEXT: "#C4C4CC;",
+        BACKGROUND_TEXT: "#7C7C8A;"
+    }
+}
